a093c38533cdf39e269836a7d3610c80e8cc2d53
Victor Rentea <victorrentea@gmail.com>
2021-11-08 15:04:16 +0200

    de-lombok

--- src/main/java/victor/testing/kata/nuclear/Coords.java
+++ src/main/java/victor/testing/kata/nuclear/Coords.java
@@ -1,15 +1,35 @@
 package victor.testing.kata.nuclear;
 
-import lombok.Value;
-
-@Value
 public class Coords {
-   int x;
-   int y;
-   int z;
-   int t;
+   private final int x;
+   private final int y;
+   private final int z;
+   private final int t;
 
    public Coords withT(int newT) {
       return new Coords(x, y, z, newT);
+   }
+
+   public Coords(int x, int y, int z, int t) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.t = t;
+   }
+
+   public int getX() {
+      return x;
+   }
+
+   public int getY() {
+      return y;
+   }
+
+   public int getZ() {
+      return z;
+   }
+
+   public int getT() {
+      return t;
    }
 }
